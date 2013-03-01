cb0f292c7ccf9cab2113258890da897f9d144c28
Brian Whitmer <brian@instructure.com>
2013-02-28 20:56:43 -0700

    fix twitter app to work with new APIs

--- public/tools/twitter/twitter.js
+++ public/tools/twitter/twitter.js
@@ -57,37 +57,73 @@ if(params.type) {
 } else {
   $("#header").show();
 }
-function iframeUrl() {
-  var type = $("#type").val();
-  var query = $("#query").val();
-  return location.protocol + "//" + location.host + "/tools/twitter/index.html#type=" + encodeURIComponent(type) + "&query=" + encodeURIComponent(query);
-}
 $("#add").click(function(event) {
   event.preventDefault();
   event.stopPropagation();
-  lti.resourceSelected({
-    embed_type: 'iframe',
-    url: iframeUrl(),
-    width: 500,
-    height: 300,
-    title: "Twitter list"
-  });
+  var obj = parseEmbedCode();
+  if(obj.type == 'unknown') {
+    alert("Invalid embed code");
+  } else {
+    lti.resourceSelected({
+      embed_type: 'iframe',
+      url: obj.iframe_url,
+      width: 500,
+      height: 300,
+      title: "Twitter list"
+    });
+  }
 });
+function parseEmbedCode() {
+  var code = $("#embed_code").val();
+  var $a = $("<div/>").html(code).find("a:last");
+
+  var iframe_url = location.protocol + "//" + location.host + "/tools/twitter/twitter_embed.html?";
+
+  if($a.hasClass('twitter-timeline') && $a.attr('data-widget-id')) {
+    return {
+      type: 'feed',
+      widget_id: $a.attr('data-widget-id'),
+      url: $a.attr('href'),
+      iframe_url: iframe_url + "widget_id=" + $a.attr('data-widget-id') + "&url=" + encodeURIComponent($a.attr('href'))
+    };
+  } else if($a.length) {
+    var matches = $a.attr('href').match(/^https:\/\/twitter.com\/([A-Za-z0-9_]+)\/status\/(\d+)$/);
+    if(matches[1] && matches[2]) {
+      return {
+        type: 'single',
+        tweet_id: matches[2],
+        author: matches[1],
+        iframe_url: iframe_url + "tweet_id=" + matches[2] + "&author=" + matches[1]
+      };
+    } else {
+      return {
+        type: 'unknown'
+      };
+    }
+  } else {
+      return {
+        type: 'unknown'
+      };
+  }
+}
 $("#search").submit(function(event) {
   event.preventDefault();
-  $("iframe").remove();
-  if($("#type").val() == "single") {
+  $("#preview").empty();
+  var obj = parseEmbedCode();
+  if(obj.type == "single") {
     var $iframe = $("<iframe/>", {
-      src: iframeUrl(),
+      src: obj.iframe_url,
+      style: "width: 500px; height: 300px; border: 0; overflow: auto;",
+      frameborder: 0
+    });
+  } else if(obj.type == 'feed') {
+    var $iframe = $("<iframe/>", {
+      src: obj.iframe_url,
       style: "width: 500px; height: 300px; border: 0; overflow: auto;",
       frameborder: 0
     });
   } else {
-    var $iframe = $("<iframe/>", {
-      src: iframeUrl(),
-      style: "width: 500px; height: 300px; border: 0; overflow: auto;",
-      frameborder: 0
-    });
+    alert("Invalid embed code");
   }
-  $("#header").after($iframe);
+  $("#preview").append($iframe);
 });
